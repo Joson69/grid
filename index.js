8ad239094830b354f02107fc95f4351872fbc157
Grid <86907021+Joson69@users.noreply.github.com>
2025-05-10 19:08:08 +0530

    testing 1.5 flash gemini

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ function saveUserData() {
 }
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-2-0-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 const client = new Client({
   intents: [
